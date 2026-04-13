bc54d45a267b2430f7f854e6c1259fbe659d8f8e
krlmlr <krlmlr@users.noreply.github.com>
2026-04-13 06:55:28 +0000

    vendor: Update vendored sources to igraph/igraph@7138f7d502a91099c80d11474e133bd7fa02abbe
    Date: 2026-04-13 06:47:02 +0000
    
    nfc: igraph_vector_shuffle() now returns void

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.11.0-13-ga486275c2"
+#define IGRAPH_VERSION "0.11.0-14-g7138f7d50"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 11
 #define IGRAPH_VERSION_PATCH 0
-#define IGRAPH_VERSION_PRERELEASE "13-ga486275c2"
+#define IGRAPH_VERSION_PRERELEASE "14-g7138f7d50"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
