1fb54c86dcffb3d2bbc2f9375f460aa99bda6da5
krlmlr <krlmlr@users.noreply.github.com>
2026-04-13 16:35:46 +0000

    vendor: Update vendored sources to igraph/igraph@bb5d56ff77b67f2418fb8dd38d0cc9f3f24bdc16
    Date: 2026-04-13 16:21:22 +0000
    
    nfc: port igraph_permute_vertices() semantics change (0400) from next

--- src/vendor/cigraph/src/operators/permute.c
+++ src/vendor/cigraph/src/operators/permute.c
@@ -1,7 +1,6 @@
-/* -*- mode: C -*-  */
 /*
-   IGraph library.
-   Copyright (C) 2006-2020 The igraph development team
+   igraph library.
+   Copyright (C) 2006-2025  The igraph development team <igraph@igraph.org>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -14,17 +13,14 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301 USA
-
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "igraph_operators.h"
+#include "igraph_isomorphism.h"
 
 #include "igraph_constructors.h"
 #include "igraph_interface.h"
-#include "igraph_isomorphism.h"
 
 #include "graph/attributes.h"
 
@@ -46,7 +42,7 @@ igraph_error_t igraph_invert_permutation(const igraph_vector_int_t *permutation,
     IGRAPH_CHECK(igraph_vector_int_resize(inverse, n));
     igraph_vector_int_fill(inverse, -1);
 
-    for (igraph_int_t i=0; i < n; i++) {
+    for (igraph_int_t i = 0; i < n; i++) {
         igraph_int_t j = VECTOR(*permutation)[i];
         if (j < 0 || j >= n) {
             IGRAPH_ERROR("Invalid index in permutation vector.", IGRAPH_EINVAL);
