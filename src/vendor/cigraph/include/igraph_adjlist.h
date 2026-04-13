c22d1c913dd840f3bd21d7f15e54e6f3729420fa
krlmlr <krlmlr@users.noreply.github.com>
2026-04-13 18:35:24 +0000

    vendor: Update vendored sources to igraph/igraph@f36d248f0ef9af24d303e03ed662fd1933a62748
    Date: 2026-04-13 17:47:35 +0000
    
    nfc: change loops argument type from igraph_bool_t to igraph_loops_t

--- src/vendor/cigraph/include/igraph_adjlist.h
+++ src/vendor/cigraph/include/igraph_adjlist.h
@@ -50,7 +50,7 @@ IGRAPH_EXPORT IGRAPH_FUNCATTR_PURE igraph_int_t igraph_adjlist_size(const igraph
 IGRAPH_EXPORT igraph_error_t igraph_adjlist_init_complementer(const igraph_t *graph,
                                                    igraph_adjlist_t *al,
                                                    igraph_neimode_t mode,
-                                                   igraph_bool_t loops);
+                                                   igraph_loops_t loops);
 IGRAPH_EXPORT igraph_error_t igraph_adjlist_init_from_inclist(
     const igraph_t *graph, igraph_adjlist_t *al, const igraph_inclist_t *il);
 IGRAPH_EXPORT void igraph_adjlist_destroy(igraph_adjlist_t *al);
