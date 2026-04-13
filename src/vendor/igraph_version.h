6a40f1fc7e252a4b23391df4c1d7f720449540d2
krlmlr <krlmlr@users.noreply.github.com>
2026-04-13 11:35:11 +0000

    vendor: Update vendored sources to igraph/igraph@7334256483ab19841c42f33d4619a5707cbb523e
    Date: 2026-04-13 11:05:30 +0000
    
    chore: ignore generated parser stubs and test temp files

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.11.0-19-g4d4897bdd"
+#define IGRAPH_VERSION "0.11.0-20-g733425648"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 11
 #define IGRAPH_VERSION_PATCH 0
-#define IGRAPH_VERSION_PRERELEASE "19-g4d4897bdd"
+#define IGRAPH_VERSION_PRERELEASE "20-g733425648"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
