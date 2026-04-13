f88575511d9ff537d3fed22ce63a167c7cbbed19
krlmlr <krlmlr@users.noreply.github.com>
2026-04-13 06:54:48 +0000

    vendor: Update vendored sources to igraph/igraph@73074ae64777741e22460c1b362ecc79adf40f5f
    Date: 2026-04-13 05:56:24 +0000
    
    vector_append: use doubling allocation strategy for append functions

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.11.0-9-gdffaae472"
+#define IGRAPH_VERSION "0.11.0-10-g73074ae64"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 11
 #define IGRAPH_VERSION_PATCH 0
-#define IGRAPH_VERSION_PRERELEASE "9-gdffaae472"
+#define IGRAPH_VERSION_PRERELEASE "10-g73074ae64"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
