a6c71a55eb710526d7e12b431749a22dfbff33e5
krlmlr <krlmlr@users.noreply.github.com>
2026-04-14 01:41:35 +0000

    vendor: Update vendored sources to igraph/igraph@7208dd94ab49552bb32731799195e7d756d70083
    Date: 2026-04-14 00:58:23 +0000
    
    nfc: rename gettype member to get_type in igraph_attribute_table_t

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.11.0-23-g6c5afe0d8"
+#define IGRAPH_VERSION "0.11.0-24-g7208dd94a"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 11
 #define IGRAPH_VERSION_PATCH 0
-#define IGRAPH_VERSION_PRERELEASE "23-g6c5afe0d8"
+#define IGRAPH_VERSION_PRERELEASE "24-g7208dd94a"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
