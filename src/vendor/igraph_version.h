4b88088e33d18b57c3c625578ec5507ec7078091
krlmlr <krlmlr@users.noreply.github.com>
2026-04-14 05:04:31 +0000

    vendor: Update vendored sources to igraph/igraph@b3210b15964320b48c31f18534e26f582cd08f50
    Date: 2026-04-14 04:19:31 +0000
    
    nfc: igraph_permute_vertices() semantics change and canonical permutation inversion

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.11.0-27-g07c362ddb"
+#define IGRAPH_VERSION "0.11.0-28-gb3210b159"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 11
 #define IGRAPH_VERSION_PATCH 0
-#define IGRAPH_VERSION_PRERELEASE "27-g07c362ddb"
+#define IGRAPH_VERSION_PRERELEASE "28-gb3210b159"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
