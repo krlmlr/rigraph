29c5cc99f44e6c5662fca0145586095245692761
krlmlr <krlmlr@users.noreply.github.com>
2026-04-14 01:41:45 +0000

    vendor: Update vendored sources to igraph/igraph@ca80b1c7cc35cf3dd2c38e788f21e29e498286f5
    Date: 2026-04-14 01:24:27 +0000
    
    nfc: attribute table get_*_*_attr members must append, not clear

--- src/vendor/igraph_version.h
+++ src/vendor/igraph_version.h
@@ -28,11 +28,11 @@
 
 __BEGIN_DECLS
 
-#define IGRAPH_VERSION "0.11.0-24-g7208dd94a"
+#define IGRAPH_VERSION "0.11.0-25-gca80b1c7c"
 #define IGRAPH_VERSION_MAJOR 0
 #define IGRAPH_VERSION_MINOR 11
 #define IGRAPH_VERSION_PATCH 0
-#define IGRAPH_VERSION_PRERELEASE "24-g7208dd94a"
+#define IGRAPH_VERSION_PRERELEASE "25-gca80b1c7c"
 
 IGRAPH_EXPORT void igraph_version(const char **version_string,
                                   int *major,
